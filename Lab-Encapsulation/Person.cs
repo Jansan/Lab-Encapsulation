86eb4d2d876ff29dd2fb26da9bdc1800ca67f385
Jansan <jansan-26@hotmail.com>
2020-08-31 16:57:00 +0200

    Person class created

---
+++ Lab-Encapsulation/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_Encapsulation
+{
+    class Person
+    {
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"FirstName: {FirstName} LastName: {LastName} Age: {Age}";
+        }
+
+
+
+
+
+
+    }
+}
